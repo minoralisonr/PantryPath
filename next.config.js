376cf70d3249be57feeaefb73081f157038fa205
Mino Ralison <rrhmino@gmail.com>
2024-08-11 23:40:26 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-import { defineConfig } from 'next';
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-};
-export default defineConfig(nextConfig);
